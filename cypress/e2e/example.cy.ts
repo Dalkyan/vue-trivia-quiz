261fffd562d12ccaa79c3558239630a485518f20
Dalkyan <36706161+Dalkyan@users.noreply.github.com>
2022-08-03 21:36:31 +0200

    Add E2E test

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -3,6 +3,28 @@
 describe("My First Test", () => {
   it("visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "You did it!");
+    cy.contains("h1", "Trivia Quiz");
+  });
+});
+
+describe("Opens the app, selects one answer for each question and submits the answers", () => {
+  it("visits the app root url and clicks the start button", () => {
+    cy.visit("/");
+    cy.contains("button", "Start").click();
+  });
+  it("Displays fetched questions and answers 9 questions", () => {
+    cy.url().should("contain", "quiz");
+    for (let index = 0; index < 9; index++) {
+      cy.get("button").first().click();
+      cy.get("button").contains("Next").click();
+    }
+  });
+  it("At 10th question picks 4th answer and is not able to click next", () => {
+    cy.get("button").eq(3).click();
+    cy.get("button").contains("Next").should("have.class", "disabled")
+  });
+  it("Is able to click submit answers and goes to summaryView", () => {
+    cy.get("a").contains("Submit").click();
+    cy.url().should("contain", "summary");
   });
 });
