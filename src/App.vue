84cd46c09a05fb5b0306126b30bf3d2f3be458af
Dalkyan <36706161+Dalkyan@users.noreply.github.com>
2022-08-05 03:03:56 +0200

    Fix screen overflowing

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ route.name == "home";
 </script>
 
 <template>
-  <div class="h-screen grid grid-rows-[min-content_auto]">
+  <div class="h-[90vh] grid grid-rows-[min-content_auto]">
     <header class="md:mt-4">
       <h1
         class="text-xl md:text-4xl mb-1 lg:mb-4 text-indigo-800 font-bold text-shadow"
