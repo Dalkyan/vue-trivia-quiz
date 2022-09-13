d87ebcf12c15722b2cc7faf7d6d26ed4c10c542d
Dalkyan <36706161+Dalkyan@users.noreply.github.com>
2022-09-13 18:20:37 +0200

    Refactor error handling and transitions

--- src/components/AsyncQuiz.vue
+++ src/components/AsyncQuiz.vue
@@ -1,89 +1,105 @@
 <template>
-  <div class="grid">
-    <div class="flex justify-center align-justify flex-col">
-      <h2>Answered {{ answeredCounter }} out of {{ array.length }}</h2>
-      <div
-        class="place-self-center w-1/2 bg-indigo-200 rounded-full h-2.5 mb-4"
-      >
+  <template v-if="isWorking">
+    <div class="grid">
+      <div class="flex justify-center align-justify flex-col">
+        <h2>Answered {{ answeredCounter }} out of {{ array.length }}</h2>
         <div
-          class="bg-orange-500 h-2.5 rounded-full"
-          :style="progressBar(answeredCounter)"
-        ></div>
-      </div>
-    </div>
-    <transition name="fade" mode="out-in">
-      <div class="min-h-max overflow-hidden" :key="questionNumber">
-        <h2 class="text-l md:text-xl">Question number {{ questionNumber }}:</h2>
-
-        <div>
-          <h3 class="text-xl lg:text-2xl m-2 lg:m-6">
-            {{
-              decodeURIComponent(array[decrease(questionNumber, 1)].question)
-            }}
-          </h3>
-        </div>
-        <div class="flex-center flex-col flex-wrap">
+          class="place-self-center w-1/2 bg-indigo-200 rounded-full h-2.5 mb-4"
+        >
           <div
-            class="flex-center w-10/12"
-            v-for="(oneAnswer, index) in allAnswers[
-              decrease(questionNumber, 1)
-            ]"
-            :key="index"
-          >
-            <button
-              class="btn w-3/4 rounded-full ml-0"
-              :class="{
-                orange:
-                  increase(index, 1) ===
-                  selectedAnswer[decrease(questionNumber, 1)],
-              }"
-              @click="selectAnswer(index)"
+            class="bg-orange-500 h-2.5 rounded-full"
+            :style="progressBar(answeredCounter)"
+          ></div>
+        </div>
+      </div>
+      <transition name="fade" mode="out-in">
+        <div class="min-h-max overflow-hidden" :key="questionNumber">
+          <h2 class="text-l md:text-xl">
+            Question number {{ questionNumber }}:
+          </h2>
+
+          <div>
+            <h3 class="text-xl lg:text-2xl m-2 lg:m-6">
+              {{
+                decodeURIComponent(array[decrease(questionNumber, 1)].question)
+              }}
+            </h3>
+          </div>
+          <div class="flex-center flex-col flex-wrap">
+            <div
+              class="flex-center w-10/12"
+              v-for="(oneAnswer, index) in allAnswers[
+                decrease(questionNumber, 1)
+              ]"
+              :key="index"
             >
-              {{ decodeURIComponent(oneAnswer) }}
-            </button>
+              <button
+                class="btn w-3/4 rounded-full ml-0"
+                :class="{
+                  orange:
+                    increase(index, 1) ===
+                    selectedAnswer[decrease(questionNumber, 1)],
+                }"
+                @click="selectAnswer(index)"
+              >
+                {{ decodeURIComponent(oneAnswer) }}
+              </button>
+            </div>
           </div>
         </div>
+      </transition>
+      <div class="nav mt-8">
+        <button
+          class="prev uppercase px-2 rounded-l-full border-2 lg:text-3xl bg-indigo-500 text-orange-300 hover:bg-indigo-800"
+          :class="{ disabled: questionNumber <= 1 }"
+          @click="questionNumber--"
+        >
+          Prev
+        </button>
+        <div class="btns">
+          <template v-for="(i, index) in array.length" :key="index">
+            <button
+              class="btns rounded-lg w-8 md:m-2 border-2 lg:text-3xl bg-indigo-200 hover:bg-indigo-800 hover:text-indigo-100 hover:-translate-y-1"
+              :class="{
+                orange: selectedAnswer[index],
+                'active-question': questionNumber === increase(index, 1),
+              }"
+              @click="questionNumber = increase(index, 1)"
+            >
+              {{ i }}
+            </button>
+          </template>
+        </div>
+        <button
+          class="next uppercase px-2 rounded-r-full border-2 lg:text-3xl bg-indigo-500 text-orange-300 hover:bg-indigo-800"
+          :class="{ disabled: questionNumber >= array.length }"
+          @click="questionNumber++"
+        >
+          Next
+        </button>
       </div>
-    </transition>
-    <div class="nav mt-8">
-      <button
-        class="prev uppercase px-2 rounded-l-full border-2 lg:text-3xl bg-indigo-500 text-orange-300 hover:bg-indigo-800"
-        :class="{ disabled: questionNumber <= 1 }"
-        @click="questionNumber--"
+      <router-link
+        to="/summary"
+        @click="submitAnswers()"
+        :class="{ disabled: answeredCounter < 10 }"
+        class="rounded-2xl self-center m-2 p-2 border-2 text-lg lg:text-3xl border-indigo-800 bg-orange-500 text-indigo-800"
       >
-        Prev
-      </button>
-      <div class="btns">
-        <template v-for="(i, index) in array.length" :key="index">
-          <button
-            class="btns rounded-lg w-8 md:m-2 border-2 lg:text-3xl bg-indigo-200 hover:bg-indigo-800 hover:text-indigo-100 hover:-translate-y-1"
-            :class="{
-              orange: selectedAnswer[index],
-              'active-question': questionNumber === increase(index, 1),
-            }"
-            @click="questionNumber = increase(index, 1)"
-          >
-            {{ i }}
-          </button>
-        </template>
-      </div>
-      <button
-        class="next uppercase px-2 rounded-r-full border-2 lg:text-3xl bg-indigo-500 text-orange-300 hover:bg-indigo-800"
-        :class="{ disabled: questionNumber >= array.length }"
-        @click="questionNumber++"
+        Submit your answers</router-link
       >
-        Next
-      </button>
+    </div>
+  </template>
+  <template v-else>
+    <div class="bg-red-600 rounded border-4 border-black sm:w-1/3 m-auto p-4">
+      <p class="font-mono shadow-white">
+        Couldn't fetch your request - please check your internet connection
+      </p>
     </div>
     <router-link
-      to="/summary"
-      @click="submitAnswers()"
-      :class="{ disabled: answeredCounter < 10 }"
-      class="rounded-2xl self-center m-2 p-2 border-2 text-lg lg:text-3xl border-indigo-800 bg-orange-500 text-indigo-800"
+      to="/"
+      class="rounded border-4 m-auto p-2 sm:w-1/3 border-black bg-indigo-500 hover:bg-indigo-800"
+      >Go back to main screen</router-link
     >
-      Submit your answers</router-link
-    >
-  </div>
+  </template>
 </template>
 
 <script setup lang="ts">
@@ -91,7 +107,6 @@ import { useQuizStore } from "@/stores/useQuizStore";
 import { storeToRefs } from "pinia";
 import { reactive, ref } from "vue";
 
-// const answerLetter = ["A", "B", "C", "D"];
 const quizStore = useQuizStore();
 const {
   answeredCounter,
@@ -102,7 +117,9 @@ const {
   questionsArray,
 } = storeToRefs(useQuizStore());
 //async function to fetch data from API
-const url = "https://opentdb.com/api.php?amount=10&encode=url3986";
+let isWorking = true;
+// const url = "https://opentdb.com/api.php?amount=10&encode=url3986";
+const url = "https://opentdb.com/";
 const fetchQuiz = async () => {
   try {
     quizStore.$reset();
@@ -110,56 +127,61 @@ const fetchQuiz = async () => {
     const quizResults = await res.json();
     console.log(res);
     console.log(quizResults.results);
+
+    const getCorrectAnswer = (index: number): string => {
+      return array.value[index].correct_answer;
+    };
+    const getIncorrectAnswers = (index: number): string[] => {
+      return array.value[index].incorrect_answers;
+    };
+    const getQuestion = (index: number): string => {
+      return array.value[index].question;
+    };
+    for (let index = 0; index < array.value.length; index++) {
+      questionsArray.value[index] = getQuestion(index);
+    }
+
+    const allAnswers = Array(10);
+    const localCorrectAnswers = Array(10);
+    for (let i = 0; i < allAnswers.length; i++) {
+      allAnswers[i] = [...getIncorrectAnswers(i), getCorrectAnswer(i)];
+      localCorrectAnswers[i] = [getCorrectAnswer(i)];
+    }
+    correctAnswers.value = localCorrectAnswers;
+
+    const selectAnswer = (number: number) => {
+      console.log(allAnswers[questionNumber.value - 1][number]);
+      if (!selectedAnswer.value[questionNumber.value - 1]) {
+        quizStore.incrementCounter();
+      }
+      selectedAnswer.value[questionNumber.value - 1] = number + 1;
+      console.log(
+        " For question number:",
+        questionNumber.value,
+        " selected answer is:",
+        selectedAnswer.value[questionNumber.value - 1]
+      );
+      myAnswers.value[questionNumber.value - 1] =
+        allAnswers[questionNumber.value - 1][number];
+    };
+    const submitAnswers = () => {
+      console.log("Answers submitted");
+      console.log(decodeURIComponent(myAnswers.value.toString()));
+      console.log("Correct answers:");
+      console.log(decodeURIComponent(correctAnswers.value.toString()));
+      console.log(decodeURIComponent(localCorrectAnswers.toString()));
+    };
+
     return quizResults.results;
   } catch (error) {
-    return "error";
+    console.error("Couldn't fetch this request");
+    isWorking = false;
+    return [];
   }
 };
-const array = ref(await fetchQuiz());
+const array = ref(await fetchQuiz()) ?? [];
 console.log(array.value);
-function getCorrectAnswer(index: number): string {
-  return array.value[index].correct_answer;
-}
-function getIncorrectAnswers(index: number): string[] {
-  return array.value[index].incorrect_answers;
-}
-function getQuestion(index: number): string {
-  return array.value[index].question;
-}
-for (let index = 0; index < array.value.length; index++) {
-  questionsArray.value[index] = getQuestion(index);
-}
 
-const allAnswers = Array(10);
-const localCorrectAnswers = Array(10);
-for (let i = 0; i < allAnswers.length; i++) {
-  allAnswers[i] = [...getIncorrectAnswers(i), getCorrectAnswer(i)];
-  localCorrectAnswers[i] = [getCorrectAnswer(i)];
-}
-correctAnswers.value = localCorrectAnswers;
-
-const selectAnswer = (number: number) => {
-  console.log(allAnswers[questionNumber.value - 1][number]);
-  if (!selectedAnswer.value[questionNumber.value - 1]) {
-    quizStore.incrementCounter();
-  }
-  selectedAnswer.value[questionNumber.value - 1] = number + 1;
-  console.log(
-    " For question number:",
-    questionNumber.value,
-    " selected answer is:",
-    selectedAnswer.value[questionNumber.value - 1]
-  );
-  myAnswers.value[questionNumber.value - 1] =
-    allAnswers[questionNumber.value - 1][number];
-};
-const submitAnswers = () => {
-  console.log("Answers submitted");
-  console.log(decodeURIComponent(myAnswers.value.toString()));
-  console.log("Correct answers:");
-  console.log(decodeURIComponent(correctAnswers.value.toString()));
-  console.log(decodeURIComponent(localCorrectAnswers.toString()));
-};
 const increase = (number: number, howMuch: number) => {
   return number + howMuch;
 };
